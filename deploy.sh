612a7471f9487e83481f7787b062906c616b309a
Ben Taitelbaum <ben@coshx.com>
2015-01-07 00:26:52 -0500

    adds some debug output

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker ps
+docker ps -f status=running
+
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p "${DOCKER_PASS}"
 for name in `docker ps -f status=running  | awk -F '\\\\s{2,}' 'NR > 1 {name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
     SHA=${CIRCLE_SHA1:0:7}
