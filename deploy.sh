b3bd17bf7a480f2681b8812b17dca0ffe29595c5
Ben Taitelbaum <ben@coshx.com>
2015-01-07 01:19:28 -0500

    limits to just containers we have built

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-docker ps
-docker ps -f status=running
-
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p "${DOCKER_PASS}"
-for name in `docker ps -f status=running  | awk -F '  +' 'NR > 1 {name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
+for name in `docker ps -f status=running | grep latest | awk -F '  +' '{name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
     SHA=${CIRCLE_SHA1:0:7}
     IMG=${CIRCLE_PROJECT_USERNAME}/$name:$SHA
 
-    docker tag $name:latest $IMG && docker push $IMG || true
+    docker tag $name:latest $IMG && docker push $IMG
 done
