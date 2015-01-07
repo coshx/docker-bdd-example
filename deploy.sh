ed072abd40aa0129f9e6abb286870092d687df4b
Ben Taitelbaum <ben@coshx.com>
2015-01-07 01:00:35 -0500

    makes delim regex compatible with awk 3.8
     - locally with awk 4.0 it worked fine

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ docker ps
 docker ps -f status=running
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p "${DOCKER_PASS}"
-for name in `docker ps -f status=running  | awk -F '\\\\s{2,}' 'NR > 1 {name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
+for name in `docker ps -f status=running  | awk -F '  +' 'NR > 1 {name=$7; sub(/_[0-9]+$/, "", name); print name}'`; do
     SHA=${CIRCLE_SHA1:0:7}
     IMG=${CIRCLE_PROJECT_USERNAME}/$name:$SHA
 
